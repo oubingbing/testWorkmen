745dfdfab2c379d615748c1103930dfa4930785f
bingbing <875307054@qq.com>
2018-05-13 14:06:11 +0800

    test

--- Applications/YourApp/Events.php
+++ Applications/YourApp/Events.php
@@ -31,14 +31,15 @@ class Events
     /**
      * 当客户端连接时触发
      * 如果业务不需此回调可以删除onConnect
-     * 
+     *
      * @param int $client_id 连接id
      */
-    public static function onConnect($client_id) {
-        // 向当前client_id发送数据 
-        Gateway::sendToClient($client_id, "Hello $client_id\n");
-        // 向所有人发送
-        Gateway::sendToAll("$client_id login\n");
+    public static function onConnect($client_id)
+    {
+        // 向当前client_id发送数据
+        Gateway::sendToClient($client_id, json_encode([
+            'clientId' => $client_id,
+        ]));
     }
     
    /**
@@ -48,7 +49,7 @@ class Events
     */
    public static function onMessage($client_id, $message) {
         // 向所有人发送 
-        Gateway::sendToAll("$client_id said $message");
+       // Gateway::sendToAll("$client_id said $message");
    }
    
    /**
@@ -57,6 +58,6 @@ class Events
     */
    public static function onClose($client_id) {
        // 向所有人发送 
-       GateWay::sendToAll("$client_id logout");
+       //GateWay::sendToAll("$client_id logout");
    }
 }
